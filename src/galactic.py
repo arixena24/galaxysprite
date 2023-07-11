9c014b318ff6a9c15e6539a651f64e65f24eca60
arixena24 <amb624@cornell.edu>
2023-07-11 15:21:25 -0400

    commit

--- src/galactic.py
+++ src/galactic.py
@@ -1,5 +1,13 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[9]:
+
+
 get_ipython().run_line_magic('run', 'main.py')
 
+
+# In[11]:
 
 
 galactic_radius = 20  ##galactic radius is fixed in kpc
@@ -17,6 +25,8 @@ def mass_density_plot(radius, density_catalog, z, title = "Mass Density"):
         plt.plot(x,y) 
         plt.title(title)
 
+
+# In[ ]:
 
 
 
